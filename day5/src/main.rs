f64030a1a7d1c5961b69da680100c7e66438110a
Nuraldin <santin.martin@gmail.com>
2024-12-16 18:55:50 -0300

    feat: solve part two of day5 by help of a youtube solution and refactor the whole day using it

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -18,8 +18,13 @@
  Add up the middle page numberes similar to previous part.
 
  Solution: 
+
+ At the beginning I went with a hashmap and a simple hashmap checking for rules.
+ I then found this answer https://www.youtube.com/watch?v=TymMkbH8e6A to solver part two which changed completely my point ofview of the problem.
+ If any number is not in the after set, i.e. that number should be after another one, it can be placed in one index.
+ After applying that algorithm it worked excellently and refactored the whole solutions. 
 */
-use std::collections::HashMap;
+use std::{collections::{HashMap,HashSet}, vec};
 use utils::{get_challenge_config, read_puzzle_input, ChallengePart};
 
 fn main() {
@@ -27,39 +32,54 @@ fn main() {
 
   let (page_ordering_rules, page_updates_list) = parse_input(challenge_config.is_test);
 
+  let mut page_updates: Vec<Vec<u32>> = Vec::new();
   match challenge_config.part {
     ChallengePart::One => {
-      let mut correct_ordering_updates = Vec::new();
       for page_update in page_updates_list {
         if check_rules(&page_ordering_rules, &page_update) {
-          correct_ordering_updates.push(page_update)
+          page_updates.push(page_update)
         }
       }
-
-      let middle_page_sum = correct_ordering_updates
-        .iter()
-        .fold(0, |accum, correct_update| accum + correct_update[correct_update.len() / 2]);
-
-      println!("Correct page update middle page sum: {}", middle_page_sum);
     },
     ChallengePart::Two => {
-      let mut corrected_incorrect_ordering_updates = Vec::new();
       for page_update in page_updates_list {
         if !check_rules(&page_ordering_rules, &page_update) {
-          corrected_incorrect_ordering_updates.push(correct_incorrect_update(page_update, page_ordering_rules.clone()));
+          page_updates.push(correct_incorrect_update(&mut page_update.clone(), &page_ordering_rules));
         }
       }
-
-      let middle_page_sum = corrected_incorrect_ordering_updates
-      .iter()
-      .fold(0, |accum, incorrect_update| accum + incorrect_update[incorrect_update.len() / 2]);
-    
-      println!("Corrected incorrect page update middle page sum: {}", middle_page_sum);
     },
   }
+
+  let middle_page_sum = page_updates
+  .iter()
+  .fold(0, |accum, update| accum + update[update.len() / 2]);
+
+  println!("Correct page update middle page sum: {}", middle_page_sum);
 }
 
-fn parse_input(is_test: bool) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
+fn check_rules(page_ordering_rules: &HashMap<u32, OrderingSets>, page_update: &Vec<u32>) -> bool {
+  for (page_idx, page) in page_update.iter().enumerate() {
+    let ordering_sets = page_ordering_rules.get(page).unwrap();
+
+    for other_page_idx in page_idx+1..page_update.len() {
+      let other_page = page_update[other_page_idx];
+
+      if ordering_sets.before.contains(&other_page) {
+        return false
+      }
+    }
+  }
+
+  true
+}
+
+#[derive(Debug)]
+struct OrderingSets {
+  before: HashSet<u32>,
+  after: HashSet<u32>
+} 
+
+fn parse_input(is_test: bool) -> (HashMap<u32, OrderingSets>, Vec<Vec<u32>>) {
   let mut page_rules = HashMap::new();
   let mut page_updates_list: Vec<Vec<u32>> = Vec::new();
   
@@ -77,82 +97,52 @@ fn parse_input(is_test: bool) -> (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>) {
   for line in read_puzzle_input(rules_file_path) {
     let page_rules_line: Vec<u32> = line.split("|").map(|s| s.parse().unwrap()).collect();
     
-      // Add an element to the array corresponding to a key
-    page_rules.entry(page_rules_line[0])
-      .or_insert(Vec::new()) // Insert an empty vector if the key doesn't exist
-      .push(page_rules_line[1]);            // Append 42 to the array
+    let before_page = page_rules_line[0];
+    let after_page = page_rules_line[1];
+
+    let before_page_rules = page_rules
+      .entry(before_page)
+      .or_insert(OrderingSets { before: HashSet::new(), after: HashSet::new()});
+    before_page_rules.after.insert(after_page);
+
+    let after_page_rules = page_rules.entry(after_page)
+      .or_insert(OrderingSets { before: HashSet::new(), after: HashSet::new()});
+    after_page_rules.before.insert(before_page);
   }
 
   (page_rules, page_updates_list)
 }
 
-fn check_rules(page_ordering_rules: &HashMap<u32, Vec<u32>>, page_update: &Vec<u32>) -> bool {
-  let mut pass = true;
+fn correct_incorrect_update(page_update: &Vec<u32>, page_ordering_rules_sets: &HashMap<u32, OrderingSets>) -> Vec<u32> {
+  let mut corrected_update = vec![];
 
-  for (page_idx, page) in page_update.iter().enumerate() {
-    if let Some(ordering_rule) = page_ordering_rules.get(page) {
-      for other_page in ordering_rule {
-        if let Some(other_page_index) = page_update.iter().position(|&page| page == *other_page) {
-          if other_page_index < page_idx {
-            pass = false;
-          }
-        }
-      }
-    }
-
-    if !pass {
-      break;
-    }
+  let mut page_set = HashSet::new();
+  for page in page_update {
+    page_set.insert(page);
   }
 
-  pass 
-}
+  while page_set.len() > 0 {
+    for page in page_set.clone() {
+      let mut placeable = true;
 
-fn correct_incorrect_update(incorrect_page_update: Vec<u32>, page_rules: HashMap<u32, Vec<u32>>) -> Vec<u32> {
-  let mut corrected_page_update = incorrect_page_update.clone();
-  let default_vector: Vec<u32> = Vec::new();
+      for other_page in page_set.clone() {
+        if page == other_page {
+          continue;
+        }
 
-  let mut corrected_page_idx = 0;
-  loop {
-    if corrected_page_idx >= corrected_page_update.len() {
-      break;
-    } 
-    let incorrect_page = corrected_page_update[corrected_page_idx];
+        if page_ordering_rules_sets.get(other_page).unwrap().after.contains(page) {
+          placeable = false;
+        }
+      }
 
-    let rules = page_rules.get(&incorrect_page).unwrap_or_else(|| &default_vector);
-    let mut rules_idx = 0;
-    loop {
-      let mut swap_flag = false; 
-
-      if rules_idx >= rules.len() {
-        corrected_page_idx += 1;
-        // println!("Finished checking page: {}, corrected state: {:?}", incorrect_page, corrected_page_update);
+      if placeable {
+        corrected_update.push(*page);
+        page_set.remove(page);
         break;
       }
-
-      let rule = rules[rules_idx];
-      if incorrect_page_update.contains(&rule) {
-        let rule_idx = corrected_page_update.iter().position(|&page| page == rule).unwrap();
-        let page_idx = corrected_page_update.iter().position(|&page| page == incorrect_page_update[corrected_page_idx]).unwrap();
-
-        if rule_idx < page_idx {
-          corrected_page_update.swap(rule_idx,page_idx);
-          swap_flag = true;
-          // println!("Had to correct the page update, new state: {:?}", corrected_page_update);
-        }
-      }
-
-      if !swap_flag {
-        rules_idx += 1;
-      }
     }
+  } 
 
-    if check_rules(&page_rules, &corrected_page_update) {
-      break;
-    }
-  }
-
-  println!("incorrect page update: {:?}, corrected page update: {:?}, rules: {:?}", incorrect_page_update, corrected_page_update, page_rules);
-
-  corrected_page_update
+  // println!("update: {page_update:?}, corrected update: {corrected_update:?}");
+  corrected_update
 }
